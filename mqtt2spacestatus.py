c54d654e528e8bfba3f1a889b875a8128a4bab36
Florian Baumann <flo@noqqe.de>
2018-03-19 15:48:17 +0100

    switch to mqtt2spacestatus

--- mqtt2spacestatus.py
+++ mqtt2spacestatus.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 import datetime
 import paho.mqtt.client as paho
 import yaml
@@ -48,22 +49,121 @@ def write_status(status, path="status.json"):
 
     return True
 
-# callback functions
-def on_message(client, userdata, msg):
-    sensors = conf["sensors"]
-    print("topic: ", msg.topic, "message: ", str(msg.payload.decode("utf-8")))
-    for sensor in sensors.keys():
-        if msg.topic == sensors[sensor]["topic"]:
-            if sensors[sensor]["msg_type"] == "json":
-                resp = json.loads(msg.payload.decode("utf-8"))
-                resp = resp[sensors[sensor]["src_field"]]
-            else:
-                try:
-                    resp = msg.payload.decode("utf-8").split()[1]
-                except IndexError:
-                    resp = msg.payload.decode("utf-8").split()[0]
+def handler_temperature(msg):
+    """
+    Parses temperature message from topic
+    and sends back formatted json data for status.json
+    :msg: message object from mqtt
+    :returns: dict
+    """
 
-            status.update({sensors[sensor]["dst_field"]: resp})
+    resp = json.loads(msg.payload.decode("utf-8"))
+    resp = resp["temperature"] / 100
+    resp = { "temperature": resp }
+    return resp
+
+def handler_sound(msg):
+    """
+    Parses sound message from topic
+    and sends back formatted json data for status.json
+    :msg: message object from mqtt
+    :returns: dict
+    """
+
+    resp = json.loads(msg.payload.decode("utf-8"))
+    resp = resp["intensity"]
+    resp = { "sound": resp }
+    return resp
+
+def handler_light(msg):
+    """
+    Parses light message from topic
+    and sends back formatted json data for status.json
+    :msg: message object from mqtt
+    :returns: dict
+    """
+
+    resp = json.loads(msg.payload.decode("utf-8"))
+    resp = resp["uv_light"]
+    resp = { "light": resp }
+    return resp
+
+def handler_humidity(msg):
+    """
+    Parses humidity message from topic
+    and sends back formatted json data for status.json
+    :msg: message object from mqtt
+    :returns: dict
+    """
+
+    resp = json.loads(msg.payload.decode("utf-8"))
+    resp = resp["value"]
+    resp = { "humidity": resp }
+    return resp
+
+def handler_hosts(msg):
+    """
+    Parses hosts message from topic
+    and sends back formatted json data for status.json
+    :msg: message object from mqtt
+    :returns: dict
+    """
+
+    resp = msg.payload.decode("utf-8").split()[1]
+    resp = float(resp)
+    resp = { "online": resp }
+    return resp
+
+def handler_door(msg):
+    """
+    Parses door message from topic
+    and sends back formatted json data for status.json
+    :msg: message object from mqtt
+    :returns: dict
+    """
+
+    resp = json.loads(msg.payload.decode("utf-8"))
+    resp = resp["value"]
+    resp = { "door": resp }
+    return resp
+
+### On Message Parser
+def on_message(client, userdata, msg):
+    """
+    This function parses all messages coming from the subscribed mqtt topics
+    and hands them over to the correct handler
+    :client: paho mqtt object
+    :userdata:
+    :msg: paho message object
+    :returns: boolean
+    """
+
+    # debug
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "--debug":
+            print("topic: ", msg.topic, "message: ", str(msg.payload.decode("utf-8")))
+
+    # Pattern match on topic and give message to corresponding handler
+    if msg.topic == "sensors/tischtennis/bricklet/temperature/tfj/temperature":
+        doc = handler_temperature(msg)
+    elif msg.topic == "sensors/tischtennis/bricklet/sound_intensity/voE/intensity":
+        doc = handler_sound(msg)
+    elif msg.topic == "sensors/tischtennis/bricklet/uv_light/xpa/uv_light":
+        doc = handler_light(msg)
+    elif msg.topic == "sensors/door/default/bme280/humidity":
+        doc = handler_humidity(msg)
+    elif msg.topic == "sensors/spacestatus/online":
+        doc = handler_hosts(msg)
+    elif msg.topic == "sensors/door/default/status":
+        doc = handler_door(msg)
+    else:
+        doc = {}
+
+    # update internal dict with latest data (any)
+    status.update(doc)
+
+    return True
+
 
 def on_connect(client, userdata, flags, rc):
     subscription_result = client.subscribe("sensors/#")
